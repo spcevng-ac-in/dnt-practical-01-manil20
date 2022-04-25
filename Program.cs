ff93314ef56da8935f11061ab1a858cadbfd1d3d
manil20 <104346189+manil20@users.noreply.github.com>
2022-04-25 12:47:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practial1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum = 0, i;
+            Console.WriteLine(" Enter a 2 or more digit number");
+            number = int.Parse(Console.ReadLine());
+            while (number > 0)
+            {
+                i = number % 10;
+                sum = sum + i;
+                number /= 10;
+            }
+            Console.WriteLine("Sum = " + sum);
+            Console.ReadLine();
+        }
+    }
+}
